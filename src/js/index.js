200468c491c6de44c431487687b1a8b5a5c0829a
iCaioReis <caioreis3186@gmail.com>
2024-02-26 22:17:16 -0300

    Finishing menu

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,16 @@ router.handle()
 
 window.onpopstate = () => {router.handle()}
 window.route = () => {router.route()}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var buttons = document.querySelectorAll('.menu-button');
+  
+    buttons.forEach(function (button) {
+      button.addEventListener('click', function () {
+        buttons.forEach(function (btn) {
+          btn.classList.remove('active');
+        });
+        button.classList.add('active');
+      });
+    });
+  });
